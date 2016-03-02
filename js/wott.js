6a9121e5fd6ee27e6d8f75fca243c465640645d9
EduardoAC <eduardo@dreammakerfactory.com>
2016-03-02 21:04:28 +0000

    Handle update JSON file

--- js/wott.js
+++ js/wott.js
@@ -6,19 +6,37 @@ var loadJSON = function (path, callback) {
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
+            callback(JSON.parse(xobj.responseText));
         }
     };
     xobj.send(null);
 };
 
+var writeJSON = function (path, data, callback) {
+    var json = JSON.stringify(data);
+    var encoded = btoa(json);
+    var xobj = new XMLHttpRequest();
+    xobj.open('POST', path, true); 
+    xobj.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send('json=' + encoded);
+};
+
 var initializeWott = function () {
     var jsonObject = null;
     
-    loadJSON('data/info.json', function(response){
-        // Parse JSON string into object
-        jsonObject = JSON.parse(response);
+    loadJSON('data/info.json', function(jsonObject){
         console.log(jsonObject);
+    });
+    
+    jsonObject = [{'1':1}];
+    writeJSON('server.php',jsonObject, function(response){
+       console.log(response); 
     });
 };
 
