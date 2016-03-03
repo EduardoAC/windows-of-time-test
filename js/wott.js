90cdda9cf555056c5f6792583aabee2b97f011b8
EduardoAC <eduardo@dreammakerfactory.com>
2016-03-03 11:16:28 +0000

    Insert interactivity - Add window event to create new Wott - Get data event to print out on console the JSON - Add server automatic save - Add remove item event - Improve some javascript structure

--- js/wott.js
+++ js/wott.js
@@ -1,3 +1,5 @@
+var temporaryData = [];
+var updateServerData;
 var loadJSON = function (path, callback) {
 
     var xobj = new XMLHttpRequest();
@@ -27,68 +29,133 @@ var writeJSON = function (path, data, callback) {
     xobj.send('json=' + encoded);
 };
 
-var createForm = function (item) {
-    //To avoid overwrite date values
-    var wottData = JSON.parse(JSON.stringify(item));
-    var formTemplate = JST["templates/form-wott.hbs"];
-    Date.prototype.yyyymmdd = function () {
-        var yyyy = this.getFullYear().toString();
-        var mm = (this.getMonth() + 1).toString(); // getMonth() is zero-based
-        var dd = this.getDate().toString();
-        return yyyy + "-" + (mm[1] ? mm : "0" + mm[0]) + "-" + (dd[1] ? dd : "0" + dd[0]); // padding
-    };
+var save = function(){
+    clearTimeout(updateServerData);
+    updateServerData = setTimeout(function () {
+        writeJSON('server.php', temporaryData, function (response) {
+            console.log("Complete save on server");
+        });
+    }, 500);
+}
+Date.prototype.yyyymmdd = function () {
+    var yyyy = this.getFullYear().toString();
+    var mm = (this.getMonth() + 1).toString(); // getMonth() is zero-based
+    var dd = this.getDate().toString();
+    return yyyy + "-" + (mm[1] ? mm : "0" + mm[0]) + "-" + (dd[1] ? dd : "0" + dd[0]); // padding
+};
 
-    var sDate = new Date(wottData['sdate'] * 1000);
-    var eDate = new Date(wottData['edate'] * 1000);
-    wottData['sdate'] = sDate.yyyymmdd();
-    wottData['edate'] = eDate.yyyymmdd();
+var createForm = function (wottData) {
+    //To avoid overwrite date values
+    var formTemplate = JST["templates/form-wott.hbs"];
+
+    if (wottData['sdate']) {
+        var sDate = new Date(wottData['sdate'] * 1000);
+        wottData['sdate'] = sDate.yyyymmdd();
+    }
+    if (wottData['edate']) {
+        var eDate = new Date(wottData['edate'] * 1000);
+        wottData['edate'] = eDate.yyyymmdd();
+    }
     return formTemplate(wottData);
 };
 var createViewResult = function (wottData) {
     var viewTemplate = JST["templates/view-result-wott.hbs"];
-    var letters = ['727272','f1595f','79c36a','599ad3','f9a65a','9e66ab','cd7058','d77fb3'];
+    var letters = ['727272', 'f1595f', '79c36a', '599ad3', 'f9a65a', '9e66ab', 'cd7058', 'd77fb3'];
     //1E5 because we are only interested in the difference in days
-    wottData['percent'] = parseInt(((wottData['edate'] - wottData['sdate']) / wottData['edate']) * 1E5);
+    wottData['percent'] = 0;
+    if (typeof wottData['edate'] !== "undefined" && typeof wottData['sdate'] !== "undefined") {
+        wottData['percent'] = parseInt(((wottData['edate'] - wottData['sdate']) / wottData['edate']) * 1E5);
+        wottData['percent'] = Math.min(wottData['percent'], 100);
+        wottData['percent'] = Math.max(wottData['percent'], 0);
+    }
     wottData['color'] = '#' + letters[Math.floor(Math.random() * letters.length)];
     return viewTemplate(wottData);
+};
+var removeWott = function(id){
+    var hpb   = document.getElementById('hpb-' + id);
+    var pb    = document.getElementById('pb-' + id).parentNode; //Progres bar is a group of two items
+    var fwott = document.getElementById('form-wott-' + id);
+    pb.parentNode.removeChild(pb);
+    hpb.parentNode.removeChild(hpb);
+    fwott.parentNode.removeChild(fwott);
+    temporaryData.slice(id,1);
+    save();
 };
 var render = function (arrayOfWott) {
     var length = arrayOfWott.length;
     var item, htmlForms = "", htmlViewResults = "";
     var spinners = document.getElementById("spinner-form");
     spinners.className += " hidden";
-
     for (var i = 0; i < length; i++) {
-        item = arrayOfWott[i];
+        item = JSON.parse(JSON.stringify(arrayOfWott[i]));
         item['index'] = i;
-        htmlForms += createForm(item);
         htmlViewResults += createViewResult(item);
+        htmlForms += createForm(item);
     }
 
     document.getElementById("form-collection").insertAdjacentHTML('beforeend', htmlForms);
     document.getElementById("view-results").insertAdjacentHTML('beforeend', htmlViewResults);
+    
+    var removeButtons = document.getElementById("form-collection").querySelectorAll(".remove-wott");
+    length = removeButtons.length;
+    for(var j = 0; j < length; j++){
+        removeButtons[j].addEventListener('click',removeWott.bind( null, j));
+    }
 };
+
 var domReady = function (callback) {
     document.readyState === "interactive" || document.readyState === "complete" ? callback() : document.addEventListener("DOMContentLoaded", callback);
 };
+
+
+var processData = function (id, type, value) {
+    var wottData = temporaryData[id];
+    if (type === 'wn') {
+        document.getElementById('hpb-' + id).innerHTML = value;
+        wottData['name'] = value;
+    } else {
+        var time = new Date(value).getTime();
+        if (type === 'wsd') {
+            wottData['sdate'] = time;
+        } else {
+            wottData['edate'] = time;
+        }
+        var percent = parseInt(((wottData['edate'] - wottData['sdate']) / wottData['edate']) * 1E5);
+        percent = Math.min(percent, 100);
+        percent = Math.max(percent, 0);
+        document.getElementById('pb-' + id).style.width = percent + "%";
+    }
+};
+
 var initializeWott = function () {
     var jsonObject = null;
 
     loadJSON('data/info.json', function (jsonObject) {
-        console.log(jsonObject);
+        temporaryData = jsonObject;
         render(jsonObject);
     });
-
-    jsonObject = [{'1': 1}];
-    writeJSON('server.php', jsonObject, function (response) {
-        console.log(response);
-    });
-
 };
-
-window.onload = initializeWott;
 domReady(function () {
     document.getElementById("form-collection").addEventListener('change', function (event) {
-        console.log(event.target);
+        var typeId = event.target.id.split("-");
+        processData(typeId[1], typeId[0], event.target.value);
+        save();
+    });
+
+    document.getElementById("get-data").addEventListener('click', function () {
+        console.log(JSON.stringify(temporaryData));
+    });
+    document.getElementById("add-window").addEventListener('click', function () {
+        var index = temporaryData.length;
+        temporaryData[index] = {};
+        var item = {};
+        item['index'] = index;
+        document.getElementById("form-collection").insertAdjacentHTML('beforeend', createForm(item));
+        document.getElementById("view-results").insertAdjacentHTML('beforeend', createViewResult(item));
+        document.getElementById("form-wott-"+index)
+                .querySelector(".remove-wott")
+                .addEventListener('click',removeWott.bind( null, index));
+        save();
     });
 });
+window.onload = initializeWott;
